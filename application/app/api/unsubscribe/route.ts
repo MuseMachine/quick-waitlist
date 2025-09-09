224b599672319d7c05abff45cf1f68665f03ea09
Kapilan Koch <kapilan.koch@musemachine.de>
2025-09-09 09:06:52 +0200

    feat: add unsubscribe page

---
+++ application/app/api/unsubscribe/route.ts
@@ -0,0 +1,49 @@
+import { Resend } from "resend";
+import { NextRequest, NextResponse } from "next/server";
+import { decrypt } from "@/app/lib/crypto"; // Deine Krypto-Funktionen
+
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+const audienceId = process.env.NEXT_PUBLIC_AUDIENCE_ID;
+
+export async function GET(request: NextRequest) {
+	const searchParams = request.nextUrl.searchParams;
+	const token = searchParams.get("token");
+
+	if (!token) {
+		return NextResponse.json({ error: "Missing token." }, { status: 400 });
+	}
+
+	try {
+		const decryptedData = decrypt(token);
+		const { email } = JSON.parse(decryptedData);
+
+		if (!email) {
+			throw new Error("Invalid token payload.");
+		}
+
+		const deleteUser = await resend.contacts.remove({
+			email,
+			audienceId: audienceId as string,
+		});
+
+		if (deleteUser.error !== null) {
+			if (deleteUser.error.name === "not_found") {
+				return NextResponse.json({ message: "You have been unsubscribed." });
+			}
+			throw new Error(
+				"Unsubscribe was not successfull, with the message: " +
+					deleteUser.error.message,
+			);
+		}
+		return NextResponse.json({
+			message: "You have been unsubscribed.",
+		});
+	} catch (error) {
+		console.error("Unsubscribe error:", error);
+		// Gib eine generische Fehlermeldung aus, um keine Systemdetails preiszugeben.
+		return NextResponse.json(
+			{ error: "Invalid or expired link." },
+			{ status: 400 },
+		);
+	}
+}
---
+++ application/components/Unsubscribe.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function UnsubscribeUI() {
+  const [message, setMessage] = useState("Confirmiung your ");
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const unsubscribeEmail = async () => {
+      const token = searchParams.get("token");
+      if (!token) {
+        setMessage("Invalid unsubscribe link");
+        return;
+      }
+
+      const response = await fetch(
+        `/api/unsubscribe?token=${encodeURIComponent(token)}`,
+      );
+
+      if (!response.ok || response.status !== 200) {
+        return setMessage(
+          "An error occurred during removing your subscription",
+        );
+      }
+
+      setMessage(
+        "Email has been removed successfully! You should not recieve any email from us.",
+      );
+    };
+
+    unsubscribeEmail();
+  }, [searchParams]);
+
+  return <p>{message}</p>;
+}
